9d59177b5306fde46350054d712e6b6bb9bffc8d
raphael-sm <raphael.mello1206@gmail.com>
2026-07-08 17:01:26 -0300

    aula 1 js

---
+++ js/aula1/01.js
@@ -0,0 +1,55 @@
+/*
+let raio = prompt("Digite o valor do raio")
+let area = Math.PI * Math.pow(raio, 2)
+alert("A área do cículo é " + area)
+*/
+// DIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISA
+/*
+let ano_nasc = prompt("Digite seu ano de nascimento: ")
+let idade  = (new Date().getFullYear()) - ano_nasc
+alert("Você tem " + idade + " anos")
+*/
+// DIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISA
+/*
+let idade1 = prompt("Digite a idade da primeira pessoa: ")
+let idade2 = prompt("Digite a idade da segunda pessoa: ")
+if (idade1 == idade2) {
+    alert("Ambas tem a mesma idade")
+} else if (Math.max(idade1, idade2) == idade1) {
+    alert("A pessoa 1 é mais velha")
+} else if (Math.max(idade1, idade2) == idade2) {
+    alert("A pessoa 2 é mais velha")
+} else {
+    alert("erro desconhecido")
+}
+*/
+// DIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISA
+/*
+let cat1 = prompt("Valor do cateto 1:")
+let cat2 = prompt("Valor do cateto 2:")
+let hipo = Math.hypot(cat1, cat2)
+alert("A hipotenusa é igual a " + hipo)
+*/
+// DIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISA
+/*
+let preco1 = prompt("Digite o preço do produto: ")
+let desconto = (preco1 - (preco1*0.15)).toFixed(2)
+alert("O preço com desconto é " + desconto)
+*/
+// DIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISA
+/*
+let celcius = prompt("digite a temperatura em celsius")
+let f = (celcius*(9/5)+32)
+alert("A temperatura em celsius é " + f)
+*/
+// DIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISA
+/*
+alert("O número aleatório é " + Math.floor(Math.random()*100))
+*/
+// DIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISA
+
+let num = prompt("Digite um número: ")
+let TmC = (num*3)-5
+alert("Esse número triplicado menos cinco é igual a " + TmC)
+
+// DIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISADIVISA
